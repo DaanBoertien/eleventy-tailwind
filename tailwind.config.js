cefe2e3bffd741a384887d76e09e02b5939dbcf9
daanb <daan.boertien@gmail.com>
2023-05-01 12:25:52 +0200

    fixed mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./_site/**/*.{html,njk}"],
   theme: {
     extend: {
+      screens: {
+        xs: "320px",
+        ...defaultTheme.screens,
+    },
       colors: {
         'primary':'#AF9A59'
       },
