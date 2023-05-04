9d09e06603fcd1df09be8edbf66157b5a78b9ca9
daanb <daan.boertien@gmail.com>
2023-05-04 20:05:14 +0200

    updated google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+const colorsYaml = fs.readFileSync('./_data/colors.yml', 'utf8');
+const colors = yaml.load(colorsYaml);
+
 module.exports = {
-  content: ["./_site/**/*.{html,njk}"],
+  content: ['./_site/**/*.{html,njk}'],
   theme: {
     extend: {
       screens: {
-        xs: "320px",
+        xs: '320px',
         ...defaultTheme.screens,
-    },
+      },
       colors: {
-        'primary':'#AF9A59'
+        primary: colors.primary,
+      },
+      fontFamily: {
+        'source-sans-pro': ['Source Sans Pro', 'sans-serif'],
+        'average': ['Average', 'serif'],
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
